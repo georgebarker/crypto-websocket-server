fdb16f72c8982bcd6dc303a8405d9f973c341462
georgebarker <georgebarker1@me.com>
2021-08-01 19:50:57 +0200

    Created a SessionManager

--- src/main/java/dev/georgebarker/endpoint/CryptocurrenciesEndpoint.java
+++ src/main/java/dev/georgebarker/endpoint/CryptocurrenciesEndpoint.java
@@ -1,8 +1,12 @@
 package dev.georgebarker.endpoint;
 
+import dev.georgebarker.manager.SessionManager;
 import dev.georgebarker.model.CryptocurrencyEncoder;
 
-import javax.websocket.*;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.Set;
@@ -13,27 +17,20 @@ import java.util.Set;
 )
 public class CryptocurrenciesEndpoint {
 
-    Set<Session> sessions;
+    private final SessionManager sessionManager = new SessionManager();
 
     @OnOpen
-    public void onOpen(Session session) throws IOException {
-        System.out.println("Opened session: " + session);
-        sessions.add(session);
-        // Get session and WebSocket connection
+    public void onOpen(Session session) {
+        sessionManager.add(session);
     }
 
-/*    @OnMessage
-    public void onMessage(Session session, Message message) throws IOException {
-        // Handle new messages
-    }*/
-
     @OnClose
-    public void onClose(Session session) throws IOException {
-        // WebSocket connection closes
+    public void onClose(Session session) {
+        sessionManager.remove(session);
     }
 
     @OnError
     public void onError(Session session, Throwable throwable) {
-        // Do error handling here
+        sessionManager.remove(session, throwable);
     }
 }
---
+++ src/main/java/dev/georgebarker/manager/SessionManager.java
@@ -0,0 +1,29 @@
+package dev.georgebarker.manager;
+
+import javax.websocket.Session;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SessionManager {
+
+    private final Set<Session> sessions = new HashSet<>();
+
+    public void add(Session session) {
+        System.out.println("INFO: Added Session: " + session);
+        sessions.add(session);
+    }
+
+    public void remove(Session session, Throwable throwable) {
+        System.out.println("ERROR: Removing Session: " + session + ", Message: " + throwable.getMessage());
+        remove(session);
+    }
+
+    public void remove(Session session) {
+        if (sessions.contains(session)) {
+            sessions.remove(session);
+            System.out.println("INFO: Removed Session: " + session);
+        } else {
+            System.out.println("WARN: Attempted to remove Session that was not present: " + session);
+        }
+    }
+}
