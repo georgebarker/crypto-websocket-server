dd329d74e104799dfbe0955f6c8b35a7ae771631
georgebarker <georgebarker1@me.com>
2021-08-13 15:16:06 +0100

    Document the Cryptocurrency model and rename the tickSize property

--- src/main/java/dev/georgebarker/model/Cryptocurrency.java
+++ src/main/java/dev/georgebarker/model/Cryptocurrency.java
@@ -5,12 +5,35 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 
+/**
+ * Represents a cryptocurrency with live price information.
+ * Prices are related to no specific fiat currency for the moment.
+ */
 @Data
 @Builder
 public class Cryptocurrency {
+    /**
+     * The name of the cryptocurrency
+     */
     private String name;
+
+    /**
+     * The last price that was paid on the market for this cryptocurrency
+     */
     private BigDecimal last;
+
+    /**
+     * The highest price someone on the market is willing to buy this cryptocurrency for
+     */
     private BigDecimal bid;
+
+    /**
+     * The lowest price someone on the market is willing to sell this cryptocurrency for
+     */
     private BigDecimal ask;
-    private int tickSize;
+
+    /**
+     * The tick size (decimal place) of the Fiat currency for the prices this cryptocurrency is denominated in
+     */
+    private int fiatTickSize;
 }
